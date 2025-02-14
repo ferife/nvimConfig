ac839fd1f2ddc7e7ae89faa8e65340d8c40344d1
Fernando RF <ferife@icloud.com>
2025-02-14 10:03:09 -0600

    Add list of plugins to implement

--- config/default.nix
+++ config/default.nix
@@ -4,4 +4,36 @@
     ./appearance.nix
     ./basics.nix
   ];
+  # Plugins to possibly enable through nixvim
+  # A plugin to auto sort lines of text
+
+  # blink-cmp
+  # blink-cmp-dictionary
+  # blink-ripgrep
+  # clipboard-image (to use in my vault)
+  # codesnap (to take screenshots of my code)
+  # colorizer (highlights RGB hex code in that color. Useful for html/css)
+  # csvview - nicely displays csv files as actual tables
+  # glow
+  # harpoon
+  # image.nvim
+  # indent-blankline
+  # lsp-lines?
+  # lspsaga
+  # nvim-jdtls
+  # numbertoggle
+  # obsidian.nvim
+  # render-markdown
+  # smear-cursor
+  # spider
+  # todo-comments
+  # trouble
+  # ts-autotag - For working with HTML tags
+  # twilight - dims inactive portions of code being edited
+    # Has command to toggle
+  # undotree
+  # vim-be-good # The plugin collection of games by ThePrimeagen designed to practice basic vim movements
+  # which-key
+  # zotcite
+    # Zotero integration when working on markdown files
 }
