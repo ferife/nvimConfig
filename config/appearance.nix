4d053ec04eb88ba728e8a5b50c70554c7d86daf7
Fernando RF <ferife@icloud.com>
2025-02-17 12:25:21 -0600

    Switch color scheme to tokyonight

--- config/appearance.nix
+++ config/appearance.nix
@@ -1,15 +1,19 @@
 {
-  # colorschemes.everforest = {
-  #   enable = true;
-  #   settings = {
-  #     enable_italic = 1;
-  #     dim_inactive_windows = 1;
-  #     transparent_background = 1;
-  #   };
-  # };
+  colorschemes.everforest = {
+    # enable = true;
+    settings = {
+      enable_italic = 1;
+      dim_inactive_windows = 1;
+      transparent_background = 1;
+    };
+  };
   colorschemes.onedark = {
-    enable = true;
+    # enable = true;
     settings.style = "cool";
+  };
+  colorschemes.tokyonight = {
+    enable = true;
+    settings.style = "storm";
   };
   plugins = {
     cursorline = {
