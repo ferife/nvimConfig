d40e224f787923e1f0de5d871ec4a8d8d73bb240
Fernando RF <ferife@icloud.com>
2025-02-25 11:26:46 -0600

    Begin configuring git plugins

--- config/plugins/nixvimPlugins/git.nix
+++ config/plugins/nixvimPlugins/git.nix
@@ -1,2 +1,15 @@
 {
+  plugins = {
+    gitsigns = {
+      # enable = true;
+      # settings.trouble = true;
+      settings = {
+        signs = {
+          delete = {
+            show_count = true;
+          };
+        };
+      };
+    };
+  };
 }
