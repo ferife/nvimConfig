294c3f99539344c1c7e53b169a623388b37efbeb
Fernando RF <ferife@icloud.com>
2025-03-20 08:08:26 -0500

    Add custom specialArgs

--- flake.nix
+++ flake.nix
@@ -9,44 +9,53 @@
 
   outputs =
     { nixvim, flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+    let
+      userSettings = {
+        username = "fernandorf";
+      };
+      systemSettings = {
+        flake = "/home/${userSettings.username}/Documents/Configs/nvimConfig/nvimConfig-main";
+      };
+    in
+      flake-parts.lib.mkFlake { inherit inputs; } {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
 
-      perSystem =
-        { system, pkgs, self', lib, ... }:
-        let
-          nixvimLib = nixvim.lib.${system};
-          nixvim' = nixvim.legacyPackages.${system};
-          nixvimModule = {
-            # inherit pkgs;
-            inherit system; # or alternatively, set `pkgs`
-            module = import ./config; # import the module directly
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
-            extraSpecialArgs = {
-              # inherit (inputs) foo;
+        perSystem =
+          { system, pkgs, self', lib, ... }:
+          let
+            nixvimLib = nixvim.lib.${system};
+            nixvim' = nixvim.legacyPackages.${system};
+            nixvimModule = {
+              # inherit pkgs;
+              inherit system; # or alternatively, set `pkgs`
+              module = import ./config; # import the module directly
+              # You can use `extraSpecialArgs` to pass additional arguments to your module files
+              extraSpecialArgs = {
+                inherit userSettings;
+                inherit systemSettings;
+              };
+            };
+            nvim = nixvim'.makeNixvimWithModule nixvimModule;
+          in
+            {
+            checks = {
+              # Run `nix flake check .` to verify that your config is not broken
+              default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+              # default = nixvimLib.check.mkTestDerivationFromNixvimModule {
+              #   inherit nvim;
+              #   name = "nixvim configuration";
+              # };
+            };
+
+            packages = {
+              # Lets you run `nix run .` to start nixvim
+              default = nvim;
             };
           };
-          nvim = nixvim'.makeNixvimWithModule nixvimModule;
-        in
-        {
-          checks = {
-            # Run `nix flake check .` to verify that your config is not broken
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-            # default = nixvimLib.check.mkTestDerivationFromNixvimModule {
-            #   inherit nvim;
-            #   name = "nixvim configuration";
-            # };
-          };
-
-          packages = {
-            # Lets you run `nix run .` to start nixvim
-            default = nvim;
-          };
-        };
-    };
+      };
 }
