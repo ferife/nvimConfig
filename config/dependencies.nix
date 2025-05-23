f71432c74e2ca512aa543fef22d11680ad62ff9e
Fernando RF <ferife@icloud.com>
2025-05-23 08:30:33 -0500

    Add nvim dependencies

--- config/dependencies.nix
+++ config/dependencies.nix
@@ -2,7 +2,14 @@
   dependencies = {
     coreutils.enable = true;
     gcc.enable = true;
-    ripgrep.enable = true;
+    # gh.enable = true; # Perform github code reviews, PRs, etc from within nvim
+    git.enable = true;
+    # imagemagick.enable = true; # DONT ACTIVATE UNTIL MARKDOWN STUFF IS FIGURED OUT
+    # lazygit.enable = true; # To be enabled if I decide to use the nvim plugin
+    # manix.enable = true;
     nodejs.enable = true;
+    # plantuml.enable = true; # UML Diagrams
+    ripgrep.enable = true;
+    tree-sitter.enable = true;
   };
 }
